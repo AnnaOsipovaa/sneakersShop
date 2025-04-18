47a95e72eb93dec77c08c839db9d2d11047380e6
Anna Osipova <a.osipova.a@yandex.ru>
2025-04-18 08:42:01 +0300

    добавила проверку аворизации для марщрутов и title

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,32 +4,49 @@ import Favorites from '../page/Favorites.vue';
 import Login from '../page/Login.vue';
 import Signup from '../page/Signup.vue';
 import Profile from '../page/Profile.vue';
+import { StorageUtils } from '../utils/storage-utils';
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'catalog',
-        component: Catalog
+        component: Catalog,
+        meta: {
+            title: 'Каталог'
+        }
     },
     {
         path: '/favorites',
         name: 'favorites',
-        component: Favorites
+        component: Favorites,
+        meta: {
+            title: 'Избранное'
+        }
     },
     {
         path: '/profile',
         name: 'profile',
-        component: Profile
+        component: Profile,
+        meta: {
+            title: 'Профиль',
+            requiresAuth: true,
+        }
     },
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
+        meta: {
+            title: 'Авторизация'
+        }
     },
     {
         path: '/signup',
         name: 'signup',
-        component: Signup
+        component: Signup,
+        meta: {
+            title: 'Регистрация'
+        }
     },
     {
         path: '/:pathMatch(.*)*',
@@ -42,3 +59,20 @@ export const router: Router = createRouter({
     history: createWebHistory(),
     routes: routes
 }); 
+
+router.afterEach((to) => {
+    const defaultTitle: string = 'Vue Sneakers';
+    document.title = <string>to.meta.title || defaultTitle
+});
+
+router.beforeEach((to, from, next) => {
+    const noTokens: boolean = !StorageUtils.getAuthToken(StorageUtils.accessTokenKey) || !StorageUtils.getAuthToken(StorageUtils.refreshTokenKey); 
+    
+    if (to.meta.requiresAuth && noTokens) {
+        next({ name: 'login' });
+    } else {
+        next();
+    }
+})
+
+ 
